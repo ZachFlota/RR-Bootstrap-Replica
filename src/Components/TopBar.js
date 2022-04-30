c734ebdd5ff2084a236fa1fe1cff16bcd3407ef9
Zachary Flota <zflota1@gmail.com>
2022-04-30 14:29:00 -0400

    add bootstrap

---
+++ src/Components/TopBar.js
@@ -0,0 +1,24 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+
+export default function TopBar() {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg" >
+                <Navbar.Brand style={{margin: "5px 0 0 20px"}} href="#home">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Instacart_logo_and_wordmark.svg/1280px-Instacart_logo_and_wordmark.svg.png" alt="logo" /> 
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto"> 
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#LogIn">Log In</Nav.Link>
+                        <Button variant="success">Sign Up</Button>{' '}
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    )
+}
